bdff86c32496dcbad0cca67e0bb3cc52ca0e0518
Constantin ARMENIADES <carmenia@e1r4p5.42.fr>
2018-08-14 17:08:23 +0200

    merge

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,16 @@
-#include "printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmenia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 17:06:42 by carmenia          #+#    #+#             */
+/*   Updated: 2018/08/14 17:06:50 by carmenia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
 
 void	ft_sort_format(t_arg *a)
 {
@@ -38,28 +50,43 @@ void	ft_start_parsing(t_arg *a)
 {
 	while (CUR && a->error != 1)
 	{
+		a->var = NULL;
 		while (CUR != '%' && CUR != '\0')
 		{
-			ft_putchar(*a->format);
-			a->format++;
+			ft_putchar(CUR);
+			a->cur++;
 			a->len++;
 		}
-		if (*a->format == '%')
+		if (CUR == '%')
 		{
+			a->cur++;
 			ft_sort_options(a);
 			ft_sort_format(a);
-			printf("%s \n,a->format");
 		}
-		a->var = NULL;
 	}
 }
 
 void init_s_arg(t_arg *a, const char *format)
 {
+	int	i;
+
+	i = 0;
 	a->cur = 0;
 	a->len = 0;
 	a->error = 0;
 	a->format = format;
+
+	while (a->flag[i])
+	{
+		a->flag[i] = 0;
+		i++;
+	}
+	i = 0;
+	while (a->size[i])
+	{
+		a->size[i] = 0;
+		i++;
+	}
 }
 
 int	ft_printf(const char *format, ...)
