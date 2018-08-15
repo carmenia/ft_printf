f01f165342026e3f8ae19c6840c14fed60818fb0
Vincent DUONG <vduong@e2r9p3.42.fr>
2018-08-15 15:54:39 +0200

    get normed kid

--- ft_str.c
+++ ft_str.c
@@ -6,12 +6,11 @@
 /*   By: vduong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 14:17:34 by vduong            #+#    #+#             */
-/*   Updated: 2018/08/15 15:47:50 by vduong           ###   ########.fr       */
+/*   Updated: 2018/08/15 15:51:35 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 void	ft_str(t_arg *a)
 {
@@ -21,7 +20,7 @@ void	ft_str(t_arg *a)
 
 	a->var = va_arg(a->ap, char *);
 	size = strlen(a->var);
-	size = (a->dot ? (a->precision < size ? a->precision : size) : size);
+	size = (a->dot && a->precision < size ? a->precision : size);
 	spaces = ((a->width < size ? 0 : a->width - size));
 	i = 0;
 	while (i < spaces)
