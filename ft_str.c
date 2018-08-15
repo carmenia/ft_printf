ca1020d4007ffe82664c7e3da50c378434dcde46
Vincent DUONG <vduong@e2r9p3.42.fr>
2018-08-15 15:19:08 +0200

    some changes

--- ft_str.c
+++ ft_str.c
@@ -6,11 +6,12 @@
 /*   By: vduong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 14:17:34 by vduong            #+#    #+#             */
-/*   Updated: 2018/08/15 15:10:52 by carmenia         ###   ########.fr       */
+/*   Updated: 2018/08/15 15:14:24 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 void	ft_str(t_arg *a)
 {
@@ -20,8 +21,11 @@ void	ft_str(t_arg *a)
 
 	a->var = va_arg(a->ap, char *);
 	size = strlen(a->var);
+	printf("size = %d\n", size);
 	size = (size > a->precision ? a->precision : size);
+	printf("size = %d\n", size);
 	spaces = (a->width < size ? 0 : a->width - size);
+	printf("spaces = %d\n", spaces);
 	i = 0;
 	while (spaces > i++)
 		ft_print_single_char(a, ' ');
